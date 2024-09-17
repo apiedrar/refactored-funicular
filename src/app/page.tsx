5b6e405393775e79224ce37a756d1bbd201ed532
Alejandro Piedra Rios <159655906+apiedrar@users.noreply.github.com>
2024-09-16 23:15:29 -0600

    create payment page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 import Image from "next/image";
-import { EnvelopeIcon } from "@heroicons/react/16/solid";
+import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify- items-center">
           <h3 className="text-xl md:text-3xl">¡Bievenido, estimado cliente!</h3>
-          <div className="flex flex-row my-[10%]">
-            <Image
-              className="block self-center"
-              src="/T2pagos_logoCuadrado_rojo.svg"
-              alt="Logo T1pagos"
-              width={300}
-              height={90}
-              priority
-            />
+          <div className="flex flex-row justify-center items-baseline my-[10%]">
+            <h1 className="inline text-7xl font-black text-red-500">T1</h1>
+            <h2 className="inline text-5xl font-bold text-gray-500">Pagos</h2>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center w-[300px] md:w-[400px] border border-current border-t-4 border-t-red-600 rounded-t">
@@ -23,29 +17,23 @@ export default function Home() {
             Autenticarse para iniciar sesión
           </h4>
           <form action="" className="flex flex-col justify-center items-center">
-            <div className="flex flex-row justify-between items-center border bg-white border-current w-[90%] md:w-[320px] mx-[20%] mb-[2%] rounded py-[7px] px-[30px]">
+            <div className="flex flex-row justify-between items-center border bg-white border-current w-[90%] md:w-[320px] mx-[20%] mb-[2%] rounded py-[7px] px-[10px]">
               <input
                 placeholder="Email"
                 name="nombredeusuario"
                 type="email"
-                className="block bg-transparent"
+                className="inline p-[2%] font-normal text-black bg-transparent w-full h-full"
               />
-              <Image
-                aria-hidden
-                src="https://nextjs.org/icons/email.svg"
-                alt="File icon"
-                width={16}
-                height={16}
-              />
+              <EnvelopeIcon className="inline size-6 text-gray-400" />
             </div>
-            <div className="flex flex-row justify-between items-center border bg-white border-current w-[90%] md:w-[320px] mx-[20%] mb-[2%] rounded py-[7px] px-[30px]">
+            <div className="flex flex-row justify-between items-center border bg-white border-current w-[90%] md:w-[320px] mx-[20%] mb-[2%] rounded py-[7px] px-[10px]">
               <input
                 placeholder="Contraseña"
                 name="contrasenia"
                 type="password"
-                className="block bg-transparent"
+                className="inline p-[2%] font-normal text-black bg-transparent w-full h-full"
               />
-              <ArrowUpRightIcon className="size-6 inline" />
+              <LockClosedIcon className="inline size-6 text-gray-400" />
             </div>
             <button className="block bg-red-600 w-[90%] md:w-[320px] px-[25%] py-[2%] mx-[10%] mb-[5%] rounded">
               Acceder
